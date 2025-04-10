7ed3220db7e929eadb7a51f66d3310d371c922b3
adamveris2024 <adamveris2024@gmail.com>
2025-04-09 22:38:48 -0700

    Before deleting Card Sets integration

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    [Route ("create")]
+    public class ProductController : Controller
+    {
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create([FromForm] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            return RedirectToAction("Success", product);
+        }
+
+        [Route("success")]
+        public IActionResult Success(Product product)
+        {
+            return View(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class Product
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
